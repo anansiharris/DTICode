79df737bcb9d35ef1ec093189932d9325d6d9825
Jack Harris <john.r.harris.ii@gmail.com>
2025-12-12 18:12:29 +0000

    Clean Repos

--- Celigo/Celigo/_original/utils/math.js
+++
@@ -1,9 +0,0 @@
-// utils/math.js
-function sumByField(records, field) {
-  return records.reduce((total, r) => {
-    const val = Number(r[field]);
-    return total + (isNaN(val) ? 0 : val);
-  }, 0);
-}
-
-module.exports = { sumByField };
